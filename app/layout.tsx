e3834d5a8b8795a2d206cb0560f4fce8723b8f65
dhwanil005 <dhwanil.shah00@gmail.com>
2024-01-23 17:38:45 -0500

    Navbar basic implemented, will work on Auth next

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Nunito } from "next/font/google";
+import Navbar from "./components/navbar/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        </body>
     </html>
   );
 }
