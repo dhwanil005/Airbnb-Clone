226daa184dd679159db3bb1f2fb90f34c506bbf8
dhwanil005 <dhwanil.shah00@gmail.com>
2024-01-28 19:25:29 -0500

    Fixed listing display

--- app/page.tsx
+++ app/page.tsx
@@ -24,8 +24,8 @@ const Home = async ({ searchParams }: HomeProps) => {
   return (
     <ClientOnly>
       <Container>
-        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          <div>
+        <div className="pt-24 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        
             {listings.map((listing) => {
               return (
                 <ListingCard
@@ -35,7 +35,7 @@ const Home = async ({ searchParams }: HomeProps) => {
                 />
               );
             })}
-          </div>
+          
         </div>
       </Container>
     </ClientOnly>
